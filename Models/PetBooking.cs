0500a9aa2cc20cbb534eae63fa45f56be3fc459d
brunoruy7 <brunoruy79@gmail.com>
2022-12-28 10:35:32 -0300

    initial commit

---
+++ Models/PetBooking.cs
@@ -0,0 +1,10 @@
+﻿namespace PetBookingApi.Models
+{
+    public class PetBooking
+    {
+        public int Id { get; set; }
+        public int Size { get; set; }
+        public string NomePet { get; set; }
+        public String NomeDono { get; set; }
+    }
+}
